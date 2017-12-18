213fe22e99e63c9d993d3238b35dfe551fc02273
David Flury <david@flury.email>
2017-12-18 15:51:13 +0100

    Add swagger and DemoController

---
+++ saycle.server/Controllers/DemoController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace saycle.server.Controllers
+{
+    /// <summary>
+    /// Demo controller to check demonstrate the Web-API.
+    /// </summary>
+    [Route("api/[controller]")]
+    public class DemoController : Controller
+    {
+        /// <summary>
+        /// Returns the answer to life the universe and everything. 
+        /// </summary>
+        /// <returns>42</returns>
+        [HttpGet]
+        public int Get()
+        {
+            return 42;
+        }
+
+        /// <summary>
+        /// Returns the difference to the life the universe and everything.
+        /// </summary>
+        /// <param name="id">Value to calculate the difference to 42</param>
+        /// <returns>42 - <paramref name="id"/></returns>
+        [HttpGet("{id}")]
+        public int Get(int id)
+        {
+            return 42 - id;
+        }
+
+        /// <summary>
+        /// Does absolutly nothing.
+        /// </summary>
+        /// <param name="value">Value to do nothing with</param>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        /// <summary>
+        /// Does absolutly nothing.
+        /// </summary>
+        /// <param name="id">Index of the thing to do nothing with</param>
+        /// <param name="value">Value to do nothing with</param>
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        /// <summary>
+        /// Deletes nothing.
+        /// </summary>
+        /// <param name="id">Index of the thing to do nothing with</param>
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- saycle.server/Startup.cs
+++ saycle.server/Startup.cs
@@ -1,38 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
+
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace saycle.server
 {
+    /// <summary>
+    /// Startup class which is instantiate by the runtime.
+    /// </summary>
     public class Startup
     {
+        /// <summary>
+        /// Called by runtime.
+        /// </summary>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
+        
+        /// <summary>
+        /// Application configuration values.
+        /// </summary>
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
+        /// <summary>
+        /// Called by runtime and used to add services to the container.
+        /// </summary>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-        }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "saycle.API",
+                    Description = "Public API for saycle.xyz",
+                    TermsOfService = "None",
+                    Contact = new Contact() { Email = "info@saycle.xyz", Url = "https://saycle.xyz" }
+                });
+                // Set the comments path for the Swagger JSON and UI.
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var xmlPath = Path.Combine(basePath, $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml");
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+        
+        /// <summary>
+        /// Called by runtime and used to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">Application request pipeline</param>
+        /// <param name="environment">Information about environment</param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment environment)
         {
-            if (env.IsDevelopment())
+            if (environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(o =>
+            {
+                o.SwaggerEndpoint("/swagger/v1/swagger.json", "saycle.API v1");
+            });
 
             app.UseMvc();
         }
