b3acacba6062d5366129d4029c785f98cb7d3495
David Flury <david@flury.email>
2017-12-28 14:02:52 +0100

    add initializer

---
+++ saycle.server/Data/Initializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace saycle.server.Data
+{
+    public class Initializer : DropCreateDatabaseIfModelChanges<SchoolContext>
+    {
+    }
+}
